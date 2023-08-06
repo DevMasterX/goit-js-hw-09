ef8ac50bfe2de5e50e4e4ff285bdec3e4e86f88c
DevMasterX <sokol080288@gmail.com>
2023-08-06 16:48:17 +0300

    update task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,7 +12,7 @@ function onClick(event) {
   const step = Number(document.querySelector('[name="step"]').value);
   const amount = Number(document.querySelector('[name="amount"]').value);
 
-  for (position = 1; position <= amount; position += 1) {
+  for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
